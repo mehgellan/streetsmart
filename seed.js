d3104a0e5a60816c0eadf6320540dee7337cdda6
mehgellan <baumegg@gmail.com>
2016-07-22 14:31:15 -0700

    set up seed data

--- seed.js
+++ seed.js
@@ -1,1 +1,40 @@
 var db = require('./models');
+
+var piecesList = [
+  {
+    title: 'Honey Bears',
+    type: 'Painting',
+    location: 'Duboce Dog Park, Duboce Ave, SF',
+    image: 'http://67.media.tumblr.com/4e3b8075457766516209c96c212b575b/tumblr_nyacm3tLg21sc86jyo1_1280.jpg',
+    active: true,
+    note: 'Seein these all over da city!'
+  },
+  {
+    title: 'Wildcat',
+    type: 'Mural',
+    location: 'Nopa, SF',
+    image: 'http://i.imgur.com/0ymjSbV.jpg',
+    active: true,
+    note: 'Love the geometry here'
+  },
+  {
+    title: 'Butterflies',
+    type: 'Painting',
+    location: 'Lower Haight, SF',
+    image: 'http://i.imgur.com/rPUoooH.jpg',
+    active: true,
+    note: 'Looks more like chalk than paint'
+  }
+];
+
+db.Piece.remove({}, function(err, pieces) {
+  console.log('REMOVED ALL PIECES');
+  db.Piece.create(pieces_list, function(err, savedPiece) {
+    if (err) {
+      return console.log('ERROR: ', err);
+    }
+    console.log('CREATED NEW ALBUM' + savedPieces);
+    console.log('CREATED', savedPiece.length, 'PIECES');
+    process.exit();
+  });
+});
