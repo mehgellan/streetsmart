88b4fdd380d40ebc24a760563d182a50f5ebc0fe
mehgellan <baumegg@gmail.com>
2016-07-22 13:45:37 -0700

    ready for heroku deployattempt #1

--- models/index.js
+++ models/index.js
@@ -1,5 +1,5 @@
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/streetsmart');
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/streetsmart");
 
 module.exports.Piece = require('./piece.js');
 module.exports.Artist = require('./artist.js');
