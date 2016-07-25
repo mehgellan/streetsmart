e2df8ebac27e3bc838cb0b9cfad87f963d26bfe3
mehgellan <baumegg@gmail.com>
2016-07-24 19:57:53 -0700

    able to show pieces on modal click

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -2,43 +2,102 @@ console.log('JS is linked');
 $(document).on('ready', function() {
 
   // GET ALL ARTISTS
-  $.get('/api/artists/579541fb1fa37ba22ae79990/pieces', onSuccess);
+  $.get('/api/artists', onSuccess);
 
-  // POST NEW PIECE
-  // $('#piece-form form').on('submit', function(e) {
-  //   e.preventDefault();
-  //   var formData = $(this).serialize();
-  //   console.log('FORM DATA =', formData);
-  //   $.ajax({
-  //     method: 'POST',
-  //     url: '/api/pieces',
-  //     data: formData,
-  //     success: newPieceSuccess,
-  //     error: newPieceError
-  //   });
-  //   $('form input').val('');
-  // });
+  // ADD AN ARTIST
+  $('#artist-form form').on('submit', function(e) {
+    e.preventDefault();
+    var formData = $(this).serialize();
+    $.post('/api/artists', formData, handleNewArtist);
+    $('form input').val('');
+  });
 
-  // EDIT PIECE ON CLICK
+  // DELETE AN ARTIST
+  $('#artists').on('click', '.delete-artist', handleDeleteArtist);
 
-  // SAVE EDITED PIECE ON CLICK
-
+  // ADD A PIECE
+  $('#artists').on('click', '.show-pieces', handleShowPieces);
+  $('#savePiece').on('click', handleNewPieceSave);
 
 });
 
-function onSuccess(pieces) {
-  console.log('FOUND A PIECE', pieces);
+function onSuccess(artists) {
+  console.log('FOUND AN ARTIST', artists);
+  artists.forEach(function(artist) {
+    renderArtist(artist);
+  });
+}
+
+function renderArtist(artist) {
+  console.log('RENDERING ARTIST', artist);
+  console.log('GET NAME OFF ARTIST', artist.name);
+  var artistHtml = $('#artist-template').html();
+  var templateFunction = Handlebars.compile(artistHtml);
+  var templatedArtistHtml = templateFunction(artist);
+  $('#artists').prepend(templatedArtistHtml);
+}
+
+function handleNewArtist(artist) {
+  console.log('NEW ARTIST POST', artist.name);
+  renderArtist(artist);
+}
+
+function handleDeleteArtist(e) {
+  var artistId = $(this).parents('.artist').data('artist-id');
+  console.log(artistId);
+  $.ajax({
+    method: 'DELETE',
+    url: '/api/artists/' + artistId,
+    success: deleteArtistSuccess,
+  });
+}
+
+function deleteArtistSuccess(data) {
+  var deletedArtistId = data._id;
+  console.log('DELETED ARTIST', deletedArtistId);
+  $('div[data-artist-id=' + deletedArtistId + ']').remove();
+}
+
+function handleShowPieces() {
+  console.log('SHOW PIECE CLICKED');
+  var currentArtistId = $(this).closest('.artist').data('artist-id');
+  console.log('ID', currentArtistId);
+  $('#pieceModal').data('artist-id', currentArtistId);
+  $.ajax({
+    method: 'GET',
+    url: '/api/artists/' + currentArtistId + '/pieces',
+    success: handleShowAllPiecesSuccess
+  });
+  $('#pieceModal').modal();
+}
+
+function handleShowAllPiecesSuccess(pieces) {
   pieces.forEach(function(piece) {
     renderPiece(piece);
   });
 }
 
-
 function renderPiece(piece) {
-  console.log('RENDERING PIECE', piece);
-  console.log('GET TITLE OFF PIECE', piece.title);
+  console.log(piece);
   var pieceHtml = $('#piece-template').html();
   var templateFunction = Handlebars.compile(pieceHtml);
   var templatedPieceHtml = templateFunction(piece);
-  $('#pieces').prepend(templatedPieceHtml);
+  console.log(templatedPieceHtml);
+  $('#pieces-list').prepend(templatedPieceHtml);
 }
+
+// function handleNewPieceSave(e) {
+//   e.preventDefault();
+//   var $modal = $('#pieceModal');
+//   var $titleInput = $('#title');
+//   var $typeInput = $('#type');
+//   var $imageInput = $('#image');
+//
+//   var dataToPost = {
+//     title: $titleInput.val(),
+//     type: $typeInput.val(),
+//     image: $imageInput.val()
+//   };
+//   var artistId = $modal.data('artist-id');
+//   console.log('GOT TITLE: ', title, ' AND TYPE: ', type, ' FOR ALBUM W/ ID: ', artistId);
+// }
