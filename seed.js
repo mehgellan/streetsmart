125de77f7deb3a35f265be9b7f707cccd354b692
mehgellan <baumegg@gmail.com>
2016-07-26 09:42:41 -0700

    remove commented cruft

--- seed.js
+++ seed.js
@@ -104,15 +104,6 @@ var piecesList = [
     note: null,
     artist: 'Unknown'
   },
-  // {
-  //   title: null,
-  //   type: null,
-  //   location: null,
-  //   image: null,
-  //   active: null,
-  //   note: null,
-  //   artist: null
-  // },
 ];
 
 
@@ -136,84 +127,3 @@ db.Artist.remove({}, function(err, removedArtists) {
     });
   });
 });
-
-
-// db.Artist.remove({}, function(err, removedArtists) {
-//   console.log('REMOVED ALL ARTISTS');
-//   db.Artist.create(artistsList, function(err, createdArtists) {
-//     if (err) {return console.log('create error', err);}
-//     console.log('CREATED ALL ARTISTS', createdArtists);
-//     db.Artist.findOne({name: piece.artist}, function(err, foundArtist) {
-//       console.log(foundArtist);
-//       db.Piece.create(piece, function(err, createdPiece) {
-//         console.log('Found created piece:', creatdPicreatedPieceece);
-//         db.Artist.findOne({name: piece.artist}, function(err, foundArtist) {
-//           console.log('Found updated artist:', foundArtist);
-//         });
-//       });
-//     });
-//   });
-// });
-
-// piecesList.push(new db.Piece({
-//   title: 'Wildcat',
-//   type: 'Mural',
-//   location: 'Nopa, SF',
-//   image: 'http://i.imgur.com/0ymjSbV.jpg',
-//   active: true,
-//   note: 'Love the geometry here',
-//   artist: 'Unknown'
-// }));
-//   {
-//     title: 'Wildcat',
-//     type: 'Mural',
-//     location: 'Nopa, SF',
-//     image: 'http://i.imgur.com/0ymjSbV.jpg',
-//     active: true,
-//     note: 'Love the geometry here',
-//     artist: 'Unknown'
-//   },
-//   {
-//     title: 'Butterflies',
-//     type: 'Painting',
-//     location: 'Lower Haight, SF',
-//     image: 'http://i.imgur.com/rPUoooH.jpg',
-//     active: true,
-//     note: 'Looks more like chalk than paint',
-//     artist: 'Unknown'
-//   }
-
-
-
-// db.Artist.remove({}, function(err, artists) {
-//   console.log('REMOVED ALL ARTISTS');
-//   db.Artist.create(artistsList, function(err, artists) {
-//     if (err) { return console.log(err); }
-//     console.log('RECREATED ALL ARTISTS');
-//     console.log('CREATED ', artists.length, ' ARTISTS');
-//
-//     db.Piece.remove({}, function(err, pieces) {
-//       console.log('REMOVED ALL PIECES');
-//       piecesList.forEach(function(pieceData) {
-//         var piece = new db.Piece({
-//           title: pieceData.title,
-//           type: pieceData.type,
-//           location: pieceData.location,
-//           image: pieceData.image,
-//           active: pieceData.active,
-//           note: pieceData.note
-//         });
-//         db.Artist.findOne({name: pieceData.artist}, function(err, foundArtist) {
-//           console.log('FOUND ARTIST ' + foundArtist.name + ' FOR PIECE ' + piece.title);
-//           if (err) { return console.log('ERROR', err); }
-//           piece.artist = foundArtist;
-//           piece.save(function(err, savedPiece) {
-//             if (err) { return console.log('ERROR', err); }
-//             console.log('SAVED ' + savedPiece.title + ' by ' + foundArtist);
-//           });
-//         });
-//       });
-//     });
-//
-//   });
-// });
